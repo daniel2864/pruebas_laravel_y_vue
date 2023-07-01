00c64dc968d1ae17fb767f3ccf8914eaba7ff2e8
Daniel <danielg@htdevs.com>
2023-07-01 15:14:38 -0300

    fix: importaciones faltantes

--- API_LARAVEL/test/app/Console/Commands/CheckDefectors.php
+++ API_LARAVEL/test/app/Console/Commands/CheckDefectors.php
@@ -3,6 +3,8 @@
 namespace App\Console\Commands;
 use App\Models\User; 
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\SendEmailReminder;
 
 class CheckDefectors extends Command
 {
@@ -28,10 +30,10 @@ class CheckDefectors extends Command
         
         $current_date  = date("Y-m-d H:i:s");
         $previous_date = date("Y-m-d H:i:s", strtotime($current_date."- 1 month"));
-        $data          = User::where('session_date', '>=', $previous_date)->get('email')->chunk(100);
+        $data          = User::where('session_date', '<=', $previous_date)->get('email')->chunk(100);
         $delay = now()->addMinutes(1);
         foreach ($data as $key => $value) {
-            Mail::to(value[$key]->email)->later($delay, new SendEmailReminder());
+            Mail::to($value[$key]->email)->later($delay, new SendEmailReminder());
         }
     }
 }
